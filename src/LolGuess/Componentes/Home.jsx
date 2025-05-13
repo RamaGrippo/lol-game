3e101077d4a493bbc34b9914162266e02f828612
rgrippo <r.grippo@santafeciudad.gov.ar>
2025-05-13 11:26:59 -0500

    Mejoras minimas

--- src/LolGuess/Componentes/Home.jsx
+++ src/LolGuess/Componentes/Home.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LoldleIcon from "../../assets/LoldleIcon.png";
+import Rift5Icon from "../../assets/Rift5Icon.png";
+import WordleIcon from "../../assets/WordleIcon.png";
 
 function Home() {
   return (
     <div className="app-container">
-      <h1>Bienvenido a los Juegos</h1>
+      <h1>Bienvenido a Lol Game</h1>
       <div className="grid">
-        <Link to="/wordle" className="square">
+        <Link
+          to="/wordle"
+          className="square"
+          style={{
+            backgroundImage: `url(${WordleIcon})`,
+          }}
+        >
           Wordle
         </Link>
-        <Link to="/loldle" className="square">
+        <Link
+          to="/loldle"
+          className="square"
+          style={{
+            backgroundImage: `url(${LoldleIcon})`,
+          }}
+        >
           Loldle
         </Link>
         <Link to="/lolgrid" className="square">
           LolGrid
         </Link>
-        <Link to="/rift5" className="square">
+        <Link
+          to="/rift5"
+          className="square"
+          style={{
+            backgroundImage: `url(${Rift5Icon})`,
+          }}
+        >
           Rift5
         </Link>
         <Link to="/esport5" className="square">
